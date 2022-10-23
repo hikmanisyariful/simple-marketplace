e38a80b6588a15c979ece26b9ada8468132d11e0
hikmanisyariful <syarifulfajar017@gmail.com>
2022-10-23 15:35:43 +0700

    Create view of products

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAllProduct } from "./store/actions/products";
+
+// Material UI
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
+
+// Components
+import Product from "./components/Product";
 
 export default function App() {
   const dispatch = useDispatch();
@@ -9,9 +20,47 @@ export default function App() {
   useEffect(() => {
     if (!products.data) {
       dispatch(fetchAllProduct());
-      console.log("i fire once");
     }
   }, []);
 
-  return <div>{JSON.stringify(products)}</div>;
+  return (
+    <Grid container spacing={2}>
+      <Grid xs={12}>
+        <Header>Evenelia</Header>
+      </Grid>
+      <Grid xs={12} style={{ display: "flex", justifyContent: "center", marginRight: "50px", marginTop: "50px" }}>
+        <TextField id="outlined-basic" label="Search" variant="outlined" size="small" style={{ marginRight: "5px" }} />
+        <Button variant="contained" size="medium" disabled={products.data ? false : true}>
+          Search
+        </Button>
+      </Grid>
+      <Grid xs={12} spacing={2} style={{ display: "flex", justifyContent: "center" }}>
+        {products.data ? (
+          <Grid container spacing={4} style={{ margin: "30px" }}>
+            {products.data.map((item, index) => {
+              return (
+                <Grid xs={12} sm={6} md={4} lg={3} key={index}>
+                  <Product key={index} item={item} />
+                </Grid>
+              );
+            })}
+          </Grid>
+        ) : (
+          <div style={{ paddingTop: "120px" }}>
+            <CircularProgress />
+          </div>
+        )}
+      </Grid>
+    </Grid>
+  );
 }
+
+const Header = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  height: "37px",
+  fontSize: "24px",
+}));
---
+++ src/components/Product.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import parse from "html-react-parser";
+
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, CardActions } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Collapse from "@mui/material/Collapse";
+
+export default function Product({ key, item }) {
+  const descHtml = parse(item.description);
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }} key={key}>
+      <CardActionArea sx={{ height: 450 }}>
+        <CardMedia component="img" height="250" image={item.image} alt={item.name} classes={{ objectFit: "cover" }} />
+        <CardContent>
+          <h3>{item.name}</h3>
+          <Typography gutterBottom variant="h7" component="div">
+            Price: {item.price}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <ExpandMore expand={expanded} onClick={handleExpandClick} aria-expanded={expanded} aria-label="show more">
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+      </CardActionArea>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <div>
+            <div
+              style={{ fontFamily: "roboto", fontWeight: 300, fontSize: "14px" }}
+              dangerouslySetInnerHTML={{ __html: descHtml }}
+            ></div>
+          </div>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
